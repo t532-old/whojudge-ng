949f9acd03b99ea745c1aa4893fbef7ac2676272
Xy Ren <xy.r@outlook.com>
2020-08-29 16:30:30 +0800

    Add error "notVerified" for future use

--- src/Whojudge/Api/Error.hs
+++ src/Whojudge/Api/Error.hs
@@ -4,9 +4,11 @@ module Whojudge.Api.Error where
 
 import Servant
 
-notExist, notOwner, notAdmin, wrongPassword, notLoggedIn :: ServerError
+notExist, notVerified, notOwner, notAdmin, wrongPassword, notLoggedIn :: ServerError
 
 notExist = err404 {errBody = "The resource identified by this ID does not exist."}
+
+notVerified = err401 {errBody = "Your account is not yet verified."}
 
 notOwner = err401 {errBody = "This resource does not belong to you, so you cannot perform this action."}
 
